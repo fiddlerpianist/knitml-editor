3363ea1555c23382dc4616a8c0bde1f4e4fa8cfd
fiddlerpianist <fiddlerpianist@9907fb7c-15d8-11de-896d-b39e94f1936c>
2009-03-25 23:33:33 +0000

    a bunch of manifest updates, enhancements to the wizard and editor pages, etc.

--- com.knitml.gpec.renderer.preferences.operations/src/com/knitml/gpec/renderer/preferences/operations/Activator.java
+++ com.knitml.gpec.renderer.preferences.operations/src/com/knitml/gpec/renderer/preferences/operations/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.knitml.gpec.renderer.preferences.options";
+	public static final String PLUGIN_ID = "com.knitml.gpec.renderer.preferences.operations";
 
 	// The shared instance
 	private static Activator plugin;
