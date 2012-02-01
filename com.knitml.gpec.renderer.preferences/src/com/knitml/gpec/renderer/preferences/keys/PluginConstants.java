16d8056157b5d39dd68290c194a3e16767926c80
fiddlerpianist <fiddlerpianist@gmail.com>
2012-02-01 15:03:46 +0000

    0.3.0.M2 release of the editor

---
+++ com.knitml.gpec.renderer.preferences/src/com/knitml/gpec/renderer/preferences/keys/PluginConstants.java
@@ -0,0 +1,8 @@
+package com.knitml.gpec.renderer.preferences.keys;
+
+public class PluginConstants {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.knitml.gpec.renderer.preferences";
+
+}
